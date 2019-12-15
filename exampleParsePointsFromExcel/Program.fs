d541db0498bc06441142499fb213a76160564ecf
hoangdungt2 <hoangdung@gmail.com>
2019-12-15 12:20:18 +0800

    add exampleParsePointsFromExcel

---
+++ exampleParsePointsFromExcel/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FsBACnet
+
+let exFn = "../../../bacnetFile.xlsx"
+
+[<EntryPoint>]
+let main argv =
+
+    if IO.File.Exists exFn |> not then
+        IO.Path.GetFullPath exFn
+        |> sprintf "[%s] is not exists"
+        |> failwith
+
+    // Create BACnetClient using command line argument 
+    let client = FsBACnet.getBACnetClient FsBACnet.BACnetType.BACnetIP "192.168.222.181"
+                 |> FsBACnet.attachOnIAmToClient BACnetDeviceStore.handlerOnIam 
+    // Discover the network in 10s
+    BACnetDeviceStore.buildDeviceStore client 10                           
+
+
+    BACnetPoint.parsePointsFromExcel exFn
+    |> BACnetPointStore.putPoints
+
+    // update values in BACnetPointStore
+    BACnetPointStore.updateValues client
+
+    // display all points
+    BACnetPointStore.getPoints()
+    |> List.iter (fun p -> printfn "[%s][%s] = %A" p.Name p.PointString p.Value)
+
+    printfn ""
+    0 // return an integer exit code
